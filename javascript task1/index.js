b752ca267672df52cbd4f42d9f4aceaed31ac10d
dineshkalaguri <dineshkalaguri@gmail.com>
2024-07-14 02:38:04 +0530

    Add files via upload

---
+++ javascript task1/index.js
@@ -0,0 +1,78 @@
+// Data Types //
+
+{
+    let a="dinesh"
+    console.log(typeof a,a)
+    // output:string //
+}
+
+{
+    let a=47
+    console.log(typeof a,a)
+    // output:num //
+}
+
+{
+    let a=18
+    let b=47
+    let z=a>b
+    console.log(typeof z,z)
+    // output:boolean //
+}
+
+{
+    console.log(typeof d,d)
+    var d=18
+    // output:undefined //
+}
+{
+    let a=null
+    console.log(typeof a,a)
+    // output:null //
+}
+
+// Mathematical Operators
+{
+    let a=18
+    let b=47
+    console.log('addition',a+b)
+    console.log('subtraction',a-b)
+    console.log('multiplication',a*b)
+    console.log('division',a/b)
+    console.log('modulus',a%b)
+
+    // output //
+    // addition 65 
+    //    subtraction -29
+    //    multiplication 846
+    //    division 0.3829787234042553
+    //    modulus 18 //
+}
+
+// Comparator Operators //
+{
+    let s=7
+    let d=8
+    console.log('equal to',s==d)
+    console.log('===',s===d)
+    console.log('not equal to',s!=d)
+    console.log('!==',s!==d)
+    console.log('greater than',s>d)
+    console.log('less than',s<d)
+    console.log('greater than equal to',s>=d)
+    console.log('less than equal to',s<=d)
+
+    // output //
+    //     equal to false
+    //     === false
+    //     not equal to true
+    //     !== true
+    //     greater than false
+    //     less than true
+    //     greater than equal to false
+    //     less than equal to true
+    //
+}
+
+
+
